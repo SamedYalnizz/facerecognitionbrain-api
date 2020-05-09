64dcce0b62f5aa5caed66637b80b893e3600aa1d
Samed Yalniz <s.yalniz@hotmail.de>
2020-05-09 08:07:25 +0300

    connecting database to server

--- server.js
+++ server.js
@@ -2,6 +2,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex'); 
+
+const postgres = knex({ 
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'syalniz',
+      password : '',
+      database : 'facerecognition'
+    }
+  });
+
+postgres.select('*').from('users').then(data => {
+    console.log(data);
+})
+
 
 
 
